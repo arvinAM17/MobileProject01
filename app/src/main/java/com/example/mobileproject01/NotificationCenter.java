78c08308df3932ed188fd2355bebb0a24aa8b555
shayesteh99 <shayesteharasti@gmail.com>
2019-02-28 15:59:45 +0330

    Notification Center was implemented

---
+++ app/src/main/java/com/example/mobileproject01/NotificationCenter.java
@@ -0,0 +1,39 @@
+package com.example.mobileproject01;
+
+import java.util.ArrayList;
+
+public class NotificationCenter {
+    private Message message;
+    private ArrayList<Observer> observers;
+
+    public NotificationCenter() {
+        message = new Message();
+        observers = new ArrayList<>();
+    }
+
+    public NotificationCenter(Message message) {
+        this.message = message;
+        observers = new ArrayList<>();
+    }
+
+    public void addObserver(Observer observer) {
+        if(!this.observers.contains(observer))
+            this.observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+        this.updateObservers();
+    }
+
+    private void updateObservers() {
+        for (Observer o:
+             this.observers) {
+            o.update(this.message);
+        }
+    }
+}
