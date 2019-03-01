c55d6ebda9c87c62e8c93dfc75613f40f4161da5
arvinAM17 <arvinazarmina@gmail.com>
2019-03-01 16:40:48 +0330

    some comments

--- app/src/main/java/com/example/mobileproject01/MainActivity.java
+++ app/src/main/java/com/example/mobileproject01/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mobileproject01;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,16 +14,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.numbers1);
+//        ArrayList<Integer> arr = new ArrayList<Integer>();
+//        for (int i = 1; i <= 10; i++)
+//            arr.add(Integer.valueOf(i));
+
+
+//        TextView textView = new TextView(this);
+//        textView.setText(arr.get(0));
+//        linearLayout.addView(textView);
         setContentView(R.layout.activity_main);
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.numbers);
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        for (int i = 1; i <= 10; i++)
-            arr.add(Integer.valueOf(i));
-        for (int i = 0; i < 8; i++) {
-            TextView textView = new TextView(this);
-            textView.setText(i + ". " + arr.get(i).toString());
-            linearLayout.addView(textView);
-        }
+        Log.v("array", "onCreate: Created.");
 
 //        StorageManager storageManager = new StorageManager();
 //        storageManager.save(10);
