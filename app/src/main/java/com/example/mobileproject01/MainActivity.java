ca46a2c71c20654c23dc94488ec3b49834ea8c9a
arvinAM17 <arvinazarmina@gmail.com>
2019-04-27 20:22:57 +0430

    Added RecyclerView and button

--- app/src/main/java/com/example/mobileproject01/MainActivity.java
+++ app/src/main/java/com/example/mobileproject01/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.mobileproject01;
 
+
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.StrictMode;
 import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,71 +26,39 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements Observer {
     LinearLayout linearLayout;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private boolean IsGridView = false;
 
-    NotificationCenter notificationCenter=new NotificationCenter();
-    MessageController messageController =MessageController.getInstance(MainActivity.this);
+
+    NotificationCenter notificationCenter = new NotificationCenter();
+    MessageController messageController = MessageController.getInstance(MainActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        linearLayout = (LinearLayout) findViewById(R.id.numbers);
-        Button clear = (Button) findViewById(R.id.clear);
-        Button get = (Button) findViewById(R.id.get);
-        Button refresh = (Button) findViewById(R.id.refresh);
+
+        Button changeView = (Button) findViewById(R.id.change_view);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(), 3);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        changeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IsGridView = !IsGridView;
+                if (IsGridView)
+                    recyclerView.setLayoutManager(gridLayoutManager);
+                else
+                    recyclerView.setLayoutManager(linearLayoutManager);
+            }
+        });
+
         notificationCenter.register(MainActivity.this);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         notificationCenter.dataLoaded();
-
-
-        clear.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                messageController.clear();
-                linearLayout.removeAllViews();
-            }
-        });
-
-
-        get.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                messageController.fetch(false,notificationCenter);
-
-
-            }
-        });
-
-        refresh.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                messageController.fetch(true,notificationCenter);
-
-
-
-            }
-        });
-
 
     }
 
@@ -96,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements Observer {
             TextView tv = new TextView(getApplicationContext()); // Prepare textview object programmatically
             tv.setText(Integer.toString(messageController.array.get(i)));
             tv.setId(i);
-            linearLayout.addView(tv); // Add to ViewGroup using this method
+            recyclerView.addView(tv); // Add to ViewGroup using this method
         }
     }
 
